fe482a67b346301f9d1814606f674724630e8e1b
Nadia27 <jones.nadia.l@gmail.com>
2018-04-08 00:40:41 -0400

    working scrape button

--- public/app.js
+++ public/app.js
@@ -5,12 +5,19 @@ function displayResults(data) {
     //empty first!
     $("tbody").empty();
     data.forEach(function (headline) {
-        $("tbody").append(`<tr><td>${headline.title}</td><td>${headline.url}</td></tr>`)
+        $("tbody").append(`<tr><td>${headline.title}</td><td>${headline.url}</td></tr>`);
     });
 }
 
-// First thing: ask the back end for json with all animals
+$("#scrape").on("click", function () {
+    // Do an api call to the back end for json with all animals sorted by weight
+    $.getJSON("/scrape", function (data) {
+        // Call our function to generate a table body
+        displayResults(data);
+    });
+});
+/* // First thing: ask the back end for json with all animals
 $.getJSON("/scrape", function (data) {
     // Call our function to generate a table body
     displayResults(data);
-});
+}); */
