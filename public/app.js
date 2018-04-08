7b8d7daaf7f8bbdde500cc6f1e82df5061b1af6d
Nadia27 <jones.nadia.l@gmail.com>
2018-04-08 19:20:05 -0400

    Fixed mulitple articles and cleaner code

--- public/app.js
+++ public/app.js
@@ -3,18 +3,22 @@
 function displayResults(data) {
     // Add to the table here...
     //empty first!
-    //$("card").empty();
+    $(".card").empty();
     data.forEach(function (headline) {
-        $(".card").append(`<div class="card"><div class="card-header">Featured Headline:</div><div class="card-body"><h5 class="card-title title">${headline.title}</h5><p class="card-text url">${headline.url}</p><a href="#" class="btn btn-primary">Save Article</a></div></div>`);
+        $(".container").append(`<div class="card">
+                        <div class="card-header">Featured Headline:</div>
+                        <div class="card-body">
+                        <h5 class="card-title title">${headline.title}</h5>
+                        <p class="card-text url">${headline.url}</p>
+                        <a href="#" class="btn btn-primary">Save Article</a>
+                        </div>
+                        </div>`);
     });
 
+}    
 
 
-
-
-<!-- <table id="results">
-}
-
+//When Scrape button is pressed grab new articles 
 $("#scrape").on("click", function () {
     // Do an api call to the back end for json with all animals sorted by weight
     $.getJSON("/scrape", function (data) {
